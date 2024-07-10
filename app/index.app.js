6ee9cae6bcd4db867469d6ed6291afadf642ae07
KilianPlusquellec <machiirl27@gmail.com>
2024-07-10 13:38:01 +0200

    fix : add cors
    Co-authored-by: JustineBouetard <JustineBouetard@users.noreply.github.com>

--- app/index.app.js
+++ app/index.app.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import router from './router/index.router.js';
 
@@ -5,6 +6,20 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(
+  cors({
+      origin: [
+          'http://localhost',
+          'http://localhost:5173',
+          'http://127.0.0.1:5173',
+          'http://localhost:5174',
+          'http://127.0.0.1:5174',
+      ],
+      credentials: true,
+      methods: "GET,PATCH,POST,DELETE",
+  })
+);
 
 app.use(router);
 
